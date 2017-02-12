afbbaf7f8867f46c0cb68e99eb46ab9020bdbbf0
godie <godie.mendoza@gmail.com>
2017-02-11 21:53:26 -0700

    routing 66%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,46 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }	 from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.compoent';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+  		BrowserModule, 
+  		FormsModule,
+  		RouterModule.forRoot([
+  		{
+  			path: 'heroes',
+  			component: HeroesComponent
+  		}, 
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path:'',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },{
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+
+  		]) 
+  		
+  		],
+  declarations: [ 
+  AppComponent,
+  DashboardComponent,
+  HeroDetailComponent,
+  HeroesComponent
+   
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
